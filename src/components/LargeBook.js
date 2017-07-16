2f3c2e3f0ad334fe8cc3b46916d7bce5cdc6a12c
Frances Yun <fyun668@gmail.com>
2017-07-16 11:15:31 -0700

    separate json data. scroll to top on mobile.

--- src/components/LargeBook.js
+++ src/components/LargeBook.js
@@ -8,7 +8,7 @@ export class LargeBook extends Component {
   	if (this.props.book.pages.length === this.props.pageIndex) {
   		background = {background: '#a90329', background: 'linear-gradient(rgb(103, 29, 80) 0%, rgb(0, 0, 0) 100%)'};
   		pageAddOn = (
-  			<div className = "last-page noselect">
+  			<div className = "last-page">
   				<img src={this.props.book.pages[0]} />
 	  			<a href="#manga-title"><button className="arrow-down"></button></a>
 	  			<p className="white">
@@ -20,11 +20,11 @@ export class LargeBook extends Component {
       pageAddOn = <FlipPage />;
     }
     return (
-      <div className="book large-book" 
+      <div className="book LargeBook noselect" 
            onClick={()=>this.props.onBookClick(this.props.book)}
            style={background}>
            {pageAddOn}
-           <button className="close-icon noselect" onClick={this.props.onCloseClick}>
+           <button className="close-icon" onClick={this.props.onCloseClick}>
               <img src="img/close-icon.svg" />
            </button>
       </div>
--- src/components/SmallBook.js
+++ src/components/SmallBook.js
@@ -8,15 +8,16 @@ export class SmallBook extends Component {
   	let tags = this.props.book.tags.slice(0, 2).map((tag)=><Tag tagName={tag} />);
     let selectedBook = (this.props.book === this.props.selectedBook ? " selected-book" : "");
     return (
-      <div className={"book small-book noselect" + selectedBook}
+      <div className={"book SmallBook noselect" + selectedBook}
            onClick={()=>this.props.onBookClick(this.props.book)}
            style={{backgroundImage:'url('+this.props.book.pages[0] +')'}}>
-           <div className="small-book-info">
-           		<h5>{this.props.book.name}</h5>
-           		<ul className="tags">{tags}</ul>
-              <div className = "page-flip"><button>flip page</button></div>
+           <div className="SmallBook__container">
+             <div className="SmallBook__info">
+             		<h5>{this.props.book.name}</h5>
+             		<ul className="tags">{tags}</ul>
+             </div>
+             <FlipPage />
            </div>
-           <FlipPage />
            <button className="close-icon noselect" onClick={this.props.onCloseClick}>
               <img src="img/close-icon.svg" />
            </button>
